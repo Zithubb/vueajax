350b80c2dfbc3b7f5ddd2abc929dcd391f6ef681
Zita <ambrus.zita99@gmail.com>
2022-01-25 15:07:49 +0100

    componensek, fonti

--- src/App.vue
+++ src/App.vue
@@ -1,135 +1,40 @@
 <template>
   <div id="app">
-
-    <table>
-      <thead>
-        <tr>
-          <th>Azonsító</th>
-          <th>Készítő</th>
-          <th>Méret</th>
-          <th>Ár</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="statue in statues" v-bind:key="statue.id">
-          <td>{{ statue.id }}</td>
-          <td>{{ statue.person }}</td>
-          <td>{{ statue.height }}</td>
-          <td>{{ statue.price }}</td>
-          <td>
-            <button @click="deleteStatue(statue.id)">Törlés</button>
-            <button @click="editPainting(statue.id)">Szerkesztés</button>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <input type="hidden" v-model="statue.id">
-          </td>
-          <td>
-            <input type="text" v-model="statue.person">
-          </td>
-          <td>
-            <input type="number" v-model="statue.height">
-          </td>
-          <td>
-            <input type="number" v-model="statue.price">
-          </td>
-          <td>
-            <button v-if="mod_new" @click="newStatue" :disabled="saving">Létrehoz</button>
-            <button v-if="!mod_new" @click="saveStatue" :disabled="saving">Mentés</button>
-            <button v-if="!mod_new" @click="cancelStatue" :disabled="saving">Mégse</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <button @click="loadData">Listázás</button>
+    <button v-if="route == 'Statues'" @click="goToPaintings()">Paintings</button>
+    <button v-if="route == 'Paintings'" @click="goToStatues()">Statues</button>
+    <Paintings v-if="route == 'Paintings'"/>
+    <Statues v-if="route == 'Statues'"/>
   </div>
 </template>
 
 <script>
 
+import Paintings from './components/Paintings.vue'
+import Statues from './components/Statues.vue'
+
 export default {
   name: 'App',
   components: {
+    Paintings,
+    Statues
   },
   data() {
     return {
-      mod_new: true, 
-      saving: false,
-      statue: {
-        id: null,
-        person: '',
-        height: '',
-        price: false
-      },
-      statues: []
+      route: "Paintings"
     }
   },
   methods: {
-    async loadData () {
-     let Response = await fetch('http://127.0.0.1:8000/api/statues')
-     let data = await Response.json()
-     this.statues = data
-    },
-    async deleteStatue(id) {
-      let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`, {
-        method: 'DELETE'
-      })
-      console.log(Response)
-      await this.loadData()
-    },
-    async newStatue() {
-      this.saving='disabled'
-     await fetch('http://127.0.0.1:8000/api/statues', {
-       method: 'POST',
-       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       },
-       body: JSON.stringify(this.statue) 
-     })
-     await this.loadData()
-     this.saving=false
-     this.resetForm()
-    },
-    async saveStatue() {
-      this.saving='disabled'
-     await fetch(`http://127.0.0.1:8000/api/statues/${this.statue.id}`, {
-       method: 'PATCH',
-       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       },
-       body: JSON.stringify(this.statue) 
-     })
-     await this.loadData()
-     this.saving=false
-     this.resetForm()
-    },
-    async editPainting(id) {
-      let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`)
-      let data = await Response.json()
-      this.statue = {...data};
-      this.mod_new = false
-    },
-    cancelStatue () {
-      this.resetForm()
-    },
-    resetForm() {
-      this.statue = {
-        id: null,
-        person: '',
-        height: '',
-        price: false
-      }
-      this.mod_new = true
-    }
-  },
-  mounted() {
-    this.loadData()
+    
+goToPaintings(){
+  this.route = 'Paintings';
+},
+goToStatues(){
+  this.route = 'Statues';
+}
   }
 }
 </script>
+
 
 <style>
 #app {
